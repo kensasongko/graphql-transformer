57f77ef9aad0d2f4307dba25933064d1fd91a072
Ken Sasongko <ken.sasongko@gmail.com>
2023-06-02 17:15:16 +0700

    generate construct

--- src/app.ts
+++ src/app.ts
@@ -26,6 +26,55 @@ const OUTPUT_PATH = "./output/";
 const SCHEMA_FILE_NAME = "graphql.schema";
 const SCHEMA_DIRECTORY_NAME = "schema";
 
+const TYPE_NAMES = ['Subscription', 'Mutation', 'Query'];
+
+const generateResolver = (tableName: string, fieldName: string, typeName: string): string => {
+  let output:string[] = [];
+  let auth = false;
+  let dataSource = "NONE";
+  if (TYPE_NAMES.indexOf(typeName) !== -1) {
+    auth = true;
+    output.push(generateAuthFunction(fieldName, typeName));
+    if (typeName === "MUTATION"){
+      dataSource = "AMAZON_DYNAMODB";
+    }
+
+  }
+
+  output.push(`
+  const ${typeName}_${fieldName}Resolver = new appsync.Resolver(this, 'pipeline', {
+    api,
+    dataSource: api.addNoneDataSource('none'),
+    typeName: '${typeName}',
+    fieldName: '${fieldName}',
+    requestMappingTemplate: appsync.MappingTemplate.fromString(\`
+      $util.qr($ctx.stash.put("typeName", "${typeName}"))
+      $util.qr($ctx.stash.put("fieldName", "${fieldName}"))
+      $util.qr($ctx.stash.put("conditions", []))
+      $util.qr($ctx.stash.put("metadata", {}))
+      $util.qr($ctx.stash.metadata.put("dataSourceType", "${dataSource}"))
+      $util.qr($ctx.stash.put("tableName", "${tableName}"))
+      $util.toJson({})
+  \`)
+    pipelineConfig: [${typeName}_${fieldName}AuthFunction],
+    responseMappingTemplate: appsync.MappingTemplate.fromString('$util.toJson($ctx.prev.result)')
+  });`);
+
+  // TODO: Field resolver
+  return output.join("\n");
+}
+
+const generateAuthFunction = (fieldName: string, typeName: string): string => {
+  return `
+  const ${typeName}_${fieldName}AuthFunction = new appsync.AppsyncFunction(this, 'function', {
+    name: '${typeName}_${fieldName}_auth1_function',
+    api,
+    dataSource: api.addNoneDataSource('none'),
+    requestMappingTemplate: appsync.MappingTemplate.fromFile('output/vtls/${typeName}.${fieldName}.auth.1.req'),
+    responseMappingTemplate: appsync.MappingTemplate.fromString('$util.toJson()')
+  });`;
+}
+
 const featureFlags: FeatureFlagProvider = {
   getBoolean(_: string, defaultValue: boolean | undefined) {
     if (defaultValue !== undefined) {
@@ -121,7 +170,9 @@ if (fs.existsSync(schemaFilePath)) {
   }
 }
 
+
 const out = transformer.transform(fullSchema);
+
 
 if (fs.existsSync(OUTPUT_PATH)) {
   const stats = fs.statSync(schemaDirectoryPath);
@@ -129,11 +180,10 @@ if (fs.existsSync(OUTPUT_PATH)) {
     fs.emptyDirSync(OUTPUT_PATH);
   } else {
     fs.rmSync(OUTPUT_PATH);
-    fs.mkdirSync(OUTPUT_PATH, { recursive: true });
   }
-} else {
-  fs.mkdirSync(OUTPUT_PATH, { recursive: true });
 }
+fs.mkdirSync(path.join(OUTPUT_PATH, 'vtls'), { recursive: true });
+fs.mkdirSync(path.join(OUTPUT_PATH, 'constructs'), { recursive: true });
 
 console.log("Writing: " + path.join(OUTPUT_PATH, "graphql.schema"));
 fs.writeFileSync(path.join(OUTPUT_PATH, "graphql.schema"), out.schema, {
@@ -141,8 +191,29 @@ fs.writeFileSync(path.join(OUTPUT_PATH, "graphql.schema"), out.schema, {
 });
 
 for (const key in out.resolvers) {
+  const pathToFile = path.join(OUTPUT_PATH, `vtls/${key}`);
+  console.log("Writing: " + pathToFile);
+  fs.writeFileSync(pathToFile, out.resolvers[key], {
+    flag: "w",
+  });
+}
+
+for (const key in out.stacks) {
   console.log("Writing: " + path.join(OUTPUT_PATH, key));
-  fs.writeFileSync(path.join(OUTPUT_PATH, key), out.resolvers[key], {
+  let output: string[] = [];
+  const stack = out.stacks[key]
+  for (const key2 in stack.Resources) {
+    if (stack.Resources[key2].Type === "AWS::AppSync::Resolver") {
+      //console.log(stack.Resources[key2]);
+      const resource = stack.Resources[key2];
+
+      const fieldName = resource.Properties.FieldName;
+      const typeName = resource.Properties.TypeName;
+      output.push(generateResolver(key, fieldName, typeName));
+    }
+  }
+  const pathToFile = path.join(OUTPUT_PATH, `constructs/${key}.ts`);
+  fs.writeFileSync(pathToFile, output.join("\n"), {
     flag: "w",
   });
 }
